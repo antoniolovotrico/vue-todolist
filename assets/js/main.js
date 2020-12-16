d74c33cf2e7988d35be90a9e26e1fdfcf0cdfba2
antoniolovotrico <antonio.lovotrico@icloud.com>
2020-12-16 14:53:11 +0100

    add tasks with function in methods worked

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,20 @@ let app = new Vue({
     el: "#app",
     //Create inside data section an array with to-do list quest
     data: {
-        toDo : ["Schierare formazione Fantacalcio","Esercitarsi con la chitarra","Tagliare i capelli","completare l'esercizio"],
+        toDo : [
+            "Schierare formazione Fantacalcio",
+            "Esercitarsi con la chitarra",
+            "Tagliare i capelli",
+            "completare l'esercizio",
+            
+        ],
+        newTask : "",
         // add an image to pass dinamically to the DOM
-        image : "./assets/img/todoimg.png"
+        image : "./assets/img/todoimg.png",     
     },
     methods: {
         addFunc() {
-            
+            this.toDo.push(this.newTask);
         }
     }
 })
