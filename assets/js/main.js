57aa78ab4f4342960a6d5ed5173be61a1731e972
antoniolovotrico <antonio.lovotrico@icloud.com>
2020-12-16 14:15:38 +0100

    inserted img dynamically with vue data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+//Vue Istance
 let app = new Vue({
-    el: "#app"
+    el: "#app",
+    //Create inside data section an array with to-do list quest
+    data: {
+        toDo : ["Schierare formazione Fantacalcio","Esercitarsi con la chitarra","Tagliare i capelli","completare l'esercizio"],
+        // add an image to pass dinamically to the DOM
+        image : "./assets/img/todoimg.png"
+    }
 })
