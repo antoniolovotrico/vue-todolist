211f8807843d2352d1026d823396ae88adeba835
antoniolovotrico <antonio.lovotrico@icloud.com>
2020-12-17 20:28:01 +0100

    update style css

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -8,6 +8,7 @@ let app = new Vue({
             "Esercitarsi con la chitarra",
             "Tagliare i capelli",
             "Completare l'esercizio",
+            "Inserire nuovi bonus"
             
         ],
         // create an empty key, this stands for our input in Dom 
@@ -19,27 +20,32 @@ let app = new Vue({
         // create an empty array where to push deleted tasks 
         deleteArr : []
     },
-    //create two function in methods, one to add tasks and another to remove them.
+    //create 4 function in methods
     methods: {
+        //Function to add tasks
         addFunc() {
             //Set conditions to ignore empty or to short tasks
             if (this.newTask.length > 3){
             this.toDo.push(this.newTask);
             }
         },
+        //Function to move tasks from to-do list to done list
         completeFunc(i) {
             let complete = this.toDo.splice(i,1);
             this.compleArr= [...this.compleArr,...complete];
         },
+        //Function to modify text of the tasks
         modifyFunc(i) {
             let newInp = prompt("Modifica il testo della tua Task");
             this.toDo.splice(i,1,newInp);
             
         },
+        //Function to move tasks from done list to delete list
         removeFunc(i) {
             let canc = this.compleArr.splice(i,1);
             this.deleteArr.push(...canc);
         },
+        //Function to empty the deleted list
         delAllFunc() {
             this.deleteArr.splice(0);
             
